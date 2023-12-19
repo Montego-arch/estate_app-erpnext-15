2db5243f7021a3d7068c8007d8078036030eb19d
Montego-arch <mmanuelmiles@gmail.com>
2023-12-19 14:05:01 +0100

    allow custom product template

--- estate_app/estate_app/doctype/store_product/store_product.py
+++ estate_app/estate_app/doctype/store_product/store_product.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2023, Montego and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 
 class StoreProduct(WebsiteGenerator):
-	pass
+	def get_context(self, context):
+		settings = frappe.get_cached_doc("Estate Settings")
+
+		if settings.use_custom_product_view_template:
+			custom_rendered_html = frappe.render_template(
+				settings.custom_product_view_template, 
+				{"doc": self}
+			)
+
+			context.custom_rendered_html = custom_rendered_html

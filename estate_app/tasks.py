be48e3e68692fcaad0fefa7b07b33f619ecc5a25
Montego-arch <mmanuelmiles@gmail.com>
2023-12-19 14:33:33 +0100

    changes in commits

--- estate_app/tasks.py
+++ estate_app/tasks.py
@@ -3,11 +3,7 @@ import frappe
 from frappe.integrations.utils import make_get_request, make_post_request
 
 BASE_URL =  "https://api.printrove.com/"
-<<<<<<< HEAD
-SECONDS_IN_YEAR = 364 * 24 * 60 * 60
-=======
 SECONDS_IN_YEAR = 364 * 24 * 60  * 60
->>>>>>> origin/develop
 
 
 @frappe.whitelist()
@@ -43,19 +39,15 @@ def sync_products_from_printrove():
 
 
 def get_printrove_access_token():
-<<<<<<< HEAD
+
     token = frappe.cache.get_value("printrove_access_token")
 
     if token:
-      
-
         return token
-    
-=======
     token = frappe.cache.get_value("estate_access_token")
     if token:
         return token
->>>>>>> origin/develop
+
     estate_settings = frappe.get_cached_doc("Estate Settings")
     auth_route = "api/external/token"
     response = make_post_request(
@@ -65,18 +57,11 @@ def get_printrove_access_token():
             "password": estate_settings.get_password("password"),
             },
         )
-<<<<<<< HEAD
+
     access_token = response["access_token"]
 
     frappe.cache.set_value("printrove_access_token", access_token, expires_in_sec=SECONDS_IN_YEAR)
     
     return access_token
-=======
->>>>>>> origin/develop
-    
-    access_token = response["access_token"]
 
-    frappe.cache.set_value("estate_access_token", access_token, expires_in_sec=SECONDS_IN_YEAR)
-
-    return access_token
 

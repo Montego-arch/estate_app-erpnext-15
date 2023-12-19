18a85e2c5dfdaced591a14f34500a4d324011fe3
Montego-arch <mmanuelmiles@gmail.com>
2023-12-19 14:21:21 +0100

    track category in product

--- estate_app/tasks.py
+++ estate_app/tasks.py
@@ -18,6 +18,8 @@ def sync_products_from_printrove():
         product_data = {
                 "front_mockup": product["mockup"]["front_mockup"],
                 "back_mockup":product["mockup"]["back_mockup"],
+                "printrove_category":product["product"]["name"],
+                "printrove_category_id":product["product"]["id"],
         }
         if not frappe.db.exists("Store Product", {"estate_id":product["id"]}):
             doc = frappe.get_doc({
